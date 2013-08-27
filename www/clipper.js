8aacfd147a5b3af03fe101eb21e786cfc348896f
Stuart P. Bentley <stuart@testtrack4.com>
2013-08-27 05:25:00 -0400

    Add selector signifier element, style encapsulation approaches

--- www/clipper.js
+++ www/clipper.js
@@ -54,8 +54,10 @@ targetHighlight.style.left = 0;
 targetHighlight.style.height = 0;
 targetHighlight.style.width = window.innerWidth;
 targetHighlight.style.borderBottomWidth = window.innerHeight;
+clipsnailDoc.body.appendChild(targetHighlight);
 
-
+// Creates an iframe for the content of the clip that will be posted
+// to Clipsnail.
 function createClipContext() {
   return clipsnailDoc.createElement('iframe');
 }
@@ -69,8 +71,7 @@ var clipDoc = clipFrame.contentDocument;
 
 var cursorX, cursorY;
 
-//If our last recieved mouseevent
-
+// Get the element at a certain position
 function getDocElementFromPoint(x,y) {
   
   // I doubt the clipsnailFrame's visibility would be anything other than 
@@ -103,12 +104,29 @@ function sanitizeElement(elem) {
     anchor.rel = 'nofollow';
     anchor.target ='_blank';
   });
+  
+  // Strip script elements
+  elem.getElementsByTagName('script').forEach(function(screlem){
+    var comment = "Strip script";
+    // considered including script source, content, or ID,
+    // but who wants the escapement headaches?
+    
+    //NOTE: Script elements with content could possible be replaced
+    //by <div>s with (by default / assumption) invisible styles, for the
+    //(unlikely) case somebody is designing a page that expects <script>
+    //elements to be present / potentially visible.
+    screlem.parentNode.replaceChild(screlem,
+      screlem.ownerDocument.createComment(comment));
+  });
 }
 
 function encapsulateElement(elem) {
+  // Grab all the literal content of any iframes in the element and don't
+  // hotlink them
   elem.getElementsByTagName('iframe').forEach(function(ifr){
     ifr.srcdoc = ifr.contentDocument.documentElement.outerHTML;
   });
+  
   // if this element is statically positioned, all non-statically positioned
   // child elements need to be re-oriented to be relative to this ancestor
   // (or maybe, if they're out of bounds, just scrapped?)
@@ -117,8 +135,10 @@ function encapsulateElement(elem) {
   // elements with non-static children is an edge case)
 }
 
+var clippedElems = [];
+
 function addElementToClip(elem) {
-  
+  clippedElems.push(elem);
 }
 
 function changeTarget(elem) {
@@ -134,15 +154,80 @@ function changeTarget(elem) {
   targetHighlight.style.borderRightWidth = window.innerWidth - scrollRight;
 }
 
-function clipClick(evt) {
-
-
-}
-
 function compileClip(){
   //NOTE: Not handling multi-element compilation
   var stylelem = clipDoc.createElement('style');
   
+  var encappedStyles = [];
+  
+  function encapElemStyles(elem){
+    // NOTE: This isn't going to capture alternate styles that would be present
+    // through media queries. I'm okay with this. (Do you really want clips
+    // that change layout after they've been clipped?)
+    
+    // This function is also undocumented and may be removed:
+    // https://bugs.webkit.org/show_bug.cgi?id=79653
+    
+    // Also, it has cross-origin permissions issues (the same-origin policy
+    // for sites is also, for some reason, in this instance, applied to
+    // extensions, AARGH):
+    // https://code.google.com/p/chromium/issues/detail?id=69626
+    var rules = getMatchedCSSRules(elem);
+    rules.forEach(function (rule){
+      if(~encappedStyles.indexOf(rule))
+        encappedStyles.push(rule);
+    });
+    elem.children.forEach(encapElemStyles);
+  }
+  //clippedElems.forEach(encapStyles);
+  
+  // Since doing this from the elements out is kind of wonky, we could also do
+  // it from the styles in...
+  function encapPageStyles() {
+    contentDoc.styleSheets.forEach(function(sheet){
+      sheet.cssRules.forEach(function(rule){
+        // we don't want to capture CSS rules other than style rules -
+        // or do we? (we actually probably should do something about, say,
+        // @font-face rules)
+        if(rule.type == 1) encappedStyles.push(rule);
+        
+        // You could check them against the element(s) with
+        // elem.querySelector(rule.selectorText), but that just runs into the
+        // problem described below (are you selecting from your clip document?)
+      });
+    });
+  }
+  //encapPageStyles()
+  
+  // but, ultimately, CSS selectors are fragile things, and it's not really
+  // worth wrestling to match every CSS selector's hierarchy and specificity
+  // to match an altered document structure. Both of the above approaches have
+  // a pretty glaring shortcoming: carrying over the same selector doesn't work
+  // when you're not carrying over the same structure (and, as CSS selectors
+  // get more and more complex, it becomes less and less feasible to, say, make
+  // dummy containers - and siblings - to fit the selector structure).
+  
+  // So the correct approach - for now, at least - is to just bake the computed
+  // styles into every element in the clip.
+  
+  function copyStyles(srcelem,trgelem){
+    trgelem.style.cssText = getComputedStyle(srcelem).cssText;
+    for (var i = 0; i < srcelem.children.length; i++) {
+      copyStyles(srcelem.children[i],trgelem.children[i]);
+    }
+    //TODO: figure out pseudo-elements
+    return trgelem;
+  }
+  
+  clippedElems.foreach(function(elem){
+    sanitizeElement( encapsulateElement(
+      copyStyles(elem, clipDoc.importNode(elem))));
+  });
+}
+
+function clipClick(evt) {
+
+
 }
 
 function onMousemove(evt) {
